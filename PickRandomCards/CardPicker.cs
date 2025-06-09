3601bbeea5d605b8511f52d04427ba9eac67fbf2
Nikolay <nikolya.lysenko@mail.ru>
2025-06-09 15:26:37 +0500

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PickRandomCards
+{
+    
+    
+    class CardPicker
+    {
+        private static Random rand = new Random();
+
+        private static string[] suit = new string[] { 
+            "Crosses", 
+            "diamonds",
+            "hearts",
+            "peaks"
+        };
+
+
+        public static string[] PickSomeCards(int numberOfCards) {
+
+            string[] pickedCards = new string[numberOfCards];
+
+            for (int i = 0; i < numberOfCards; i++) 
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        public static string RandomSuit() {
+            int randSuit = rand.Next(0,4);
+
+            return suit[randSuit];
+        }
+
+        public static string RandomValue() {
+
+            int randValue = rand.Next(2,15);
+
+            switch (randValue)
+            {
+                case 11:
+                    return "jack";
+                case 12:
+                    return "queen";
+                case 13:
+                    return "king";
+                case 14:
+                    return "ace";
+            }
+
+            return randValue.ToString();
+            
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.NetworkInformation;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the numbers of cards to pick: ");
+            string userNumber = Console.ReadLine();
+            if (int.TryParse(userNumber, out int numberOfCards))
+            {
+                string[] deck = CardPicker.PickSomeCards(numberOfCards);
+                
+                foreach (string card in deck)
+                {
+                    Console.WriteLine("Type of card: " + card);
+                }
+            }
+            else 
+            { 
+                Console.WriteLine("This input cannot be converted to a number");
+            }
+            
+        }
+    }
+}
+
